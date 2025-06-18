adf0610ec5e2a423e78b118a57808cc4839b01f3
Jiniyas Shah <jiniyasshahts@gmail.com>
2025-06-18 20:38:09 +0545

    Messed up with code

--- test/fake-ui.js
+++ test/fake-ui.js
@@ -1,25 +1,26 @@
 /** @module default-ui Provide a fake instagram UI. */
 
 /**
- *
+ *hello world
  * @param {Document} document
  * @returns {HTMLDivElement}
  */
 export function createMountElement(document) {
-	console.debug("createMountElement", arguments)
-	const element = document.createElement("div")
-	element.id = "mount_43243"
+	console.debug("createMountElement", arguments);
+	const element = document.createElement("div");
+	element.id = "mount_43243";
 	element.innerHTML = `
 		<div>
 			<div>
 				<div>
 					<div>
+					Hello World
 					</div>
 				</div>
 			</div>
 		</div>
-	`
-	return element
+	`;
+	return element;
 }
 
 /**
@@ -29,10 +30,14 @@ export function createMountElement(document) {
  * @param {int} [itemsPerPage=1]
  * @returns {HTMLDivElement}
  */
-export function createMessagesWrapperElement(document, totalPages=0, itemsPerPage=0) {
-	console.debug("createMessagesWrapperElement", arguments)
-	const element = document.createElement("div")
-	element.setAttribute("role", "grid")
+export function createMessagesWrapperElement(
+	document,
+	totalPages = 0,
+	itemsPerPage = 0
+) {
+	console.debug("createMessagesWrapperElement", arguments);
+	const element = document.createElement("div");
+	element.setAttribute("role", "grid");
 	element.innerHTML = `
 		<div>
 			<div>
@@ -42,48 +47,69 @@ export function createMessagesWrapperElement(document, totalPages=0, itemsPerPag
 				</div>
 			</div>
 		</div>
-	`
-	const messageWrapperElement = element.querySelector("#messageWrapper")
+	`;
+	const messageWrapperElement = element.querySelector("#messageWrapper");
 	Object.defineProperty(messageWrapperElement, "scrollTop", {
 		get() {
-			return messageWrapperElement._scrollTop
+			return messageWrapperElement._scrollTop;
 		},
 		set(newValue) {
-			messageWrapperElement._scrollTop = newValue
-			messageWrapperElement.dispatchEvent(new document.defaultView.Event("scroll"))
-		}
-	})
-	messageWrapperElement.currentPage = 0
-	for(let i =0; i < itemsPerPage;i++) {
-		const messageElement = createMessageElement.call(null, document, `Item ${i}`)
-		messageWrapperElement.append(messageElement)
+			messageWrapperElement._scrollTop = newValue;
+			messageWrapperElement.dispatchEvent(
+				new document.defaultView.Event("scroll")
+			);
+		},
+	});
+	messageWrapperElement.currentPage = 0;
+	for (let i = 0; i < itemsPerPage; i++) {
+		const messageElement = createMessageElement.call(
+			null,
+			document,
+			`Item ${i}`
+		);
+		messageWrapperElement.append(messageElement);
 	}
 	messageWrapperElement.addEventListener("scroll", (event) => {
-		if(event.target.scrollTop === 0) {
-			console.debug("scroll event")
-			const hasNextPage = event.target.currentPage + 1 <= totalPages
-			console.debug("hasNextPage", hasNextPage, event.target.currentPage, totalPages)
-			if(hasNextPage) {
-				const progressBar = document.createElement("div")
-				progressBar.setAttribute("role", "progressbar")
-				event.target.appendChild(progressBar)
-				const hasMorePages = event.target.currentPage + 2 <= totalPages
-				console.debug("hasMorePages", hasMorePages)
-				if(hasMorePages) {
-					event.target.scrollTop = 5
+		if (event.target.scrollTop === 0) {
+			console.debug("scroll event");
+			const hasNextPage = event.target.currentPage + 1 <= totalPages;
+			console.debug(
+				"hasNextPage",
+				hasNextPage,
+				event.target.currentPage,
+				totalPages
+			);
+			if (hasNextPage) {
+				const progressBar = document.createElement("div");
+				progressBar.setAttribute("role", "progressbar");
+				event.target.appendChild(progressBar);
+				const hasMorePages = event.target.currentPage + 2 <= totalPages;
+				console.debug("hasMorePages", hasMorePages);
+				if (hasMorePages) {
+					event.target.scrollTop = 5;
 				}
-				console.debug("messageWrapperElement loading page", event.target.currentPage)
-				console.debug("event.target.children.length", event.target.children.length)
-				for(let i =0; i < itemsPerPage;i++) {
-					const messageElement = createMessageElement.call(null, document, `Item ${i}`)
-					messageWrapperElement.append(messageElement)
+				console.debug(
+					"messageWrapperElement loading page",
+					event.target.currentPage
+				);
+				console.debug(
+					"event.target.children.length",
+					event.target.children.length
+				);
+				for (let i = 0; i < itemsPerPage; i++) {
+					const messageElement = createMessageElement.call(
+						null,
+						document,
+						`Item ${i}`
+					);
+					messageWrapperElement.append(messageElement);
 				}
-				progressBar.remove()
-				event.target.currentPage++
+				progressBar.remove();
+				event.target.currentPage++;
 			}
 		}
-	})
-	return element
+	});
+	return element;
 }
 
 /**
@@ -95,42 +121,60 @@ export function createMessagesWrapperElement(document, totalPages=0, itemsPerPag
  * @param {number} [eventsTimeout=0]
  * @returns {HTMLDivElement}
  */
-export function createMessageElement(document, text="", includesUnsend=true, ignored=false, eventsTimeout=0) {
-	console.debug("createMessageElement", arguments)
-	const element = document.createElement("div")
-	element.setAttribute("role", "row")
-	if(ignored) {
-		element.setAttribute("data-idmu-ignore", "true")
+export function createMessageElement(
+	document,
+	text = "",
+	includesUnsend = true,
+	ignored = false,
+	eventsTimeout = 0
+) {
+	console.debug("createMessageElement", arguments);
+	const element = document.createElement("div");
+	element.setAttribute("role", "row");
+	if (ignored) {
+		element.setAttribute("data-idmu-ignore", "true");
 	}
-	element.innerHTML = `${includesUnsend?"<span>You sent</span>":""}<span>${text}</span>`
-	element.addEventListener("mouseover", event => {
-		console.debug(`message ${text} mouseover`)
+	element.innerHTML = `${
+		includesUnsend ? "<span>You sent</span>" : ""
+	}<span>${text}</span>`;
+	element.addEventListener("mouseover", (event) => {
+		console.debug(`message ${text} mouseover`);
 		setTimeout(() => {
-			const moreElement = event.target.ownerDocument.createElement("div")
-			moreElement.setAttribute("aria-label", "See more options for message from foo")
-			event.target.appendChild(moreElement)
-			event.target.addEventListener("click", () => { // Listen for event of parent instead of moreElement because instagram use a svg Element
-				console.debug(`moreElement clicked`)
+			const moreElement = event.target.ownerDocument.createElement("div");
+			moreElement.setAttribute(
+				"aria-label",
+				"See more options for message from foo"
+			);
+			event.target.appendChild(moreElement);
+			event.target.addEventListener("click", () => {
+				// Listen for event of parent instead of moreElement because instagram use a svg Element
+				console.debug(`moreElement clicked`);
 				setTimeout(() => {
-					if(event.target.messageActionsMenuElement) {
-						event.target.messageActionsMenuElement.remove()
-						delete event.target.messageActionsMenuElement
+					if (event.target.messageActionsMenuElement) {
+						event.target.messageActionsMenuElement.remove();
+						delete event.target.messageActionsMenuElement;
 					} else {
-						const messageActionsMenuElement = createMessageActionsMenuElement(document, includesUnsend, eventsTimeout)
-						messageActionsMenuElement.messageElement = event.target
-						event.target.messageActionsMenuElement = messageActionsMenuElement
-						event.target.ownerDocument.body.appendChild(messageActionsMenuElement)
+						const messageActionsMenuElement = createMessageActionsMenuElement(
+							document,
+							includesUnsend,
+							eventsTimeout
+						);
+						messageActionsMenuElement.messageElement = event.target;
+						event.target.messageActionsMenuElement = messageActionsMenuElement;
+						event.target.ownerDocument.body.appendChild(
+							messageActionsMenuElement
+						);
 					}
-				}, eventsTimeout)
-			})
-		})
-	})
+				}, eventsTimeout);
+			});
+		});
+	});
 	element.addEventListener("mouseout", () => {
-		if(element.querySelector("[aria-label]")) {
-			element.querySelector("[aria-label]").remove()
+		if (element.querySelector("[aria-label]")) {
+			element.querySelector("[aria-label]").remove();
 		}
-	})
-	return element
+	});
+	return element;
 }
 
 /**
@@ -139,10 +183,10 @@ export function createMessageElement(document, text="", includesUnsend=true, ign
  * @returns {HTMLDivElement}
  */
 export function createDummyMessageElement(document) {
-	console.debug("createDummyMessageElement", arguments)
-	const element = document.createElement("div")
-	element.setAttribute("role", "row")
-	return element
+	console.debug("createDummyMessageElement", arguments);
+	const element = document.createElement("div");
+	element.setAttribute("role", "row");
+	return element;
 }
 
 /**
@@ -151,56 +195,58 @@ export function createDummyMessageElement(document) {
  * @param {boolean} [includesUnsend=true]
  * @returns {HTMLDivElement}
  */
-export function createMessageActionsMenuElement(document, includesUnsend=true) {
-	console.debug("createMessageActionsMenuElement", arguments)
-	const element = document.createElement("div")
-	element.setAttribute("role", "dialog")
-	const menuElement = document.createElement("div")
-	menuElement.id = "menu"
-	menuElement.setAttribute("role", "menu")
-	const menuItem1 = document.createElement("div")
-	menuItem1.setAttribute("role", "")
-	const barElement = document.createElement("div")
-	barElement.setAttribute("role", "menuitem")
-	barElement.textContent = "Bar"
-	menuItem1.appendChild(barElement)
-	const menuItem2 = document.createElement("div")
-	menuItem2.setAttribute("role", "")
-	const fooElement = document.createElement("div")
-	fooElement.setAttribute("role", "menuitem")
-	fooElement.textContent = "Foo"
-	menuItem2.appendChild(fooElement)
-	menuElement.appendChild(menuItem1)
-	menuElement.appendChild(menuItem2)
-	element.appendChild(menuElement)
-	if(includesUnsend) {
-		const menuItem3 = document.createElement("div")
-		menuItem3.setAttribute("role", "")
-		const unsendElement = document.createElement("div")
-		unsendElement.setAttribute("role", "menuitem")
-		unsendElement.textContent = "Unsend"
-		unsendElement.id = "unsend"
-		menuItem3.appendChild(unsendElement)
-		menuElement.appendChild(menuItem3)
+export function createMessageActionsMenuElement(
+	document,
+	includesUnsend = true
+) {
+	console.debug("createMessageActionsMenuElement", arguments);
+	const element = document.createElement("div");
+	element.setAttribute("role", "dialog");
+	const menuElement = document.createElement("div");
+	menuElement.id = "menu";
+	menuElement.setAttribute("role", "menu");
+	const menuItem1 = document.createElement("div");
+	menuItem1.setAttribute("role", "");
+	const barElement = document.createElement("div");
+	barElement.setAttribute("role", "menuitem");
+	barElement.textContent = "Bar";
+	menuItem1.appendChild(barElement);
+	const menuItem2 = document.createElement("div");
+	menuItem2.setAttribute("role", "");
+	const fooElement = document.createElement("div");
+	fooElement.setAttribute("role", "menuitem");
+	fooElement.textContent = "Foo";
+	menuItem2.appendChild(fooElement);
+	menuElement.appendChild(menuItem1);
+	menuElement.appendChild(menuItem2);
+	element.appendChild(menuElement);
+	if (includesUnsend) {
+		const menuItem3 = document.createElement("div");
+		menuItem3.setAttribute("role", "");
+		const unsendElement = document.createElement("div");
+		unsendElement.setAttribute("role", "menuitem");
+		unsendElement.textContent = "Unsend";
+		unsendElement.id = "unsend";
+		menuItem3.appendChild(unsendElement);
+		menuElement.appendChild(menuItem3);
 		element.querySelector("#unsend").addEventListener("click", () => {
-			console.debug("#unsend button clicked")
-			const confirmElement = document.createElement("div")
-			confirmElement.setAttribute("role", "dialog")
-			const confirmUnsend = document.createElement("button")
-			confirmUnsend.id = "confirmUnsend"
-			confirmElement.appendChild(confirmUnsend)
-			const cancelUnsend = document.createElement("button")
-			cancelUnsend.id = "cancelUnsend"
-			confirmElement.appendChild(cancelUnsend)
-			menuElement.replaceWith(confirmElement)
-			console.debug("Creating htmlConfirm modal and setting listener")
+			console.debug("#unsend button clicked");
+			const confirmElement = document.createElement("div");
+			confirmElement.setAttribute("role", "dialog");
+			const confirmUnsend = document.createElement("button");
+			confirmUnsend.id = "confirmUnsend";
+			confirmElement.appendChild(confirmUnsend);
+			const cancelUnsend = document.createElement("button");
+			cancelUnsend.id = "cancelUnsend";
+			confirmElement.appendChild(cancelUnsend);
+			menuElement.replaceWith(confirmElement);
+			console.debug("Creating htmlConfirm modal and setting listener");
 			element.querySelector("#confirmUnsend").addEventListener("click", () => {
-				console.debug("#confirmUnsend clicked")
-				element.messageElement.remove()
-				element.remove()
-			})
-		})
+				console.debug("#confirmUnsend clicked");
+				element.messageElement.remove();
+				element.remove();
+			});
+		});
 	}
-	return element
+	return element;
 }
-
